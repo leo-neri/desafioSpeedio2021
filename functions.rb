4e2391210bf03d1e7a6a93f1f9823c4240d57097
Leo Neri <leoneriribeiro@gmail.com>
2021-07-23 21:16:35 -0300

    Commit 3 - Organizing files in main and functions.

---
+++ functions.rb
@@ -0,0 +1,16 @@
+require 'open-uri'
+require 'nokogiri'
+require 'csv'
+
+
+def get_download_links
+  html = URI.open("https://www.gov.br/receitafederal/pt-br/assuntos/orientacao-tributaria/cadastros/consultas/dados-publicos-cnpj")
+  doc = Nokogiri::HTML(html)
+  final_array = []
+
+  doc.xpath('//a[contains(text(), "ESTABELECIMENTO")]').map {|element| element["href"]}.compact.each do |element|
+    final_array << element
+  end
+
+  final_array
+end
--- main.rb
+++ main.rb
@@ -1,18 +1,4 @@
-require 'open-uri'
-require 'nokogiri'
-require 'csv'
+require_relative 'functions'
 
-
-def get_download_links
-  html = URI.open("'https://www.gov.br/receitafederal/pt-br/assuntos/orientacao-tributaria/cadastros/consultas/dados-publicos-cnpj'")
-  doc = Nokogiri::HTML(html)
-  final_array = []
-
-  doc.xpath('//a[contains(text(), "ESTABELECIMENTO")]').map {|element| element["href"]}.compact.each do |element|
-    final_array << element
-  end
-
-  final_array
-end
-
-puts get_download_links
+download_links = get_download_links
+puts download_links
