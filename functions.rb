49fdeb27ec833e773ba6b2ea094c3e4aab9d64ff
Leo Neri <leoneriribeiro@gmail.com>
2021-07-25 01:37:00 -0300

    Commit 4 - Created generate_hash and store_mongo functions.

--- functions.rb
+++ functions.rb
@@ -1,16 +1,40 @@
 require 'open-uri'
 require 'nokogiri'
 require 'csv'
+require 'mongo'
+
 
 
 def get_download_links
   html = URI.open("https://www.gov.br/receitafederal/pt-br/assuntos/orientacao-tributaria/cadastros/consultas/dados-publicos-cnpj")
   doc = Nokogiri::HTML(html)
   final_array = []
-
   doc.xpath('//a[contains(text(), "ESTABELECIMENTO")]').map {|element| element["href"]}.compact.each do |element|
     final_array << element
   end
-
   final_array
+end
+
+def generate_hash(index, fields)
+  data = CSV.read("data00#{index}.csv", liberal_parsing: true,  :col_sep => ";")
+  dict = {}
+  data.each_with_index do |row, _i|
+    new_dict = {}
+    row.each_with_index do |campo, _index|
+      new_dict.store(fields[_index].to_sym, campo)
+    end
+    dict.store(_i, new_dict)
+  end
+  dict
+end
+
+def store_mongo(hash)
+  client = Mongo::Client.new('mongodb://127.0.0.1:27017/ttest2')
+  collection = client[:estabelecimentos]
+  hash.each do |key, value|
+    collection.insert_one(value)
+  end
+  # collection.find.each do |document|
+  #   puts document
+  # end
 end
--- main.rb
+++ main.rb
@@ -1,4 +1,14 @@
 require_relative 'functions'
 
-download_links = get_download_links
-puts download_links
+index = 1
+
+fields =  ['cnpj_basico', 'cnpj_ordem', 'cnpj_dv', 'identificador matriz/filial', 'nome_fantasia',
+           'situacao_cadastral', 'data_situacao_cadastral', 'motivo_situacao_cadastral',
+           'nome_da_cidade_no_exterior', 'pais', 'data_de_inicio_atividade', 'cnae_fiscal_principal',
+           'cnae_fiscal_secundaria', 'tipo_de_logradouro', 'logradouro', 'numero', 'complemento', 'bairro',
+           'cep', 'uf', 'municipio', 'ddd_1', 'telefone_1', 'ddd_2', 'telefone_2', 'ddd_do_fax', 'fax',
+           'correio_eletronico', 'situacao_especial', 'data_da_situacao_especial'
+]
+
+hash = generate_hash(index, fields)
+store_mongo(hash)
